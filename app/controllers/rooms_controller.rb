7d1844c099d95669dfaab22bb2435a0f48f100c4
ginia3150 <ginia3150@gmail.com>
2021-05-10 17:56:16 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,7 @@
 class RoomsController < ApplicationController
+  
+  def index
+  end
   def new
     @room = Room.new
   end
@@ -10,7 +13,7 @@ class RoomsController < ApplicationController
       render :new
     end
   end
-  
+
   private
   def room_params
     params.require(:room).permit(:name, user_ids: [])
